b8b9c4471fe86003849fd53b597e559f608976dd
ottowhite <whiteotto4@gmail.com>
2019-06-19 09:00:30 +0100

    implemented normalisation, sigmoid, feedforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import struct as st
 import numpy as np
-
+np.set_printoptions(linewidth=1000)
 
 def read_mnist(no_items_each):
     data_locations = {
@@ -49,25 +49,41 @@ def read_mnist(no_items_each):
     return formatted_data
 
 
-mnist = read_mnist(1000)
+mnist = read_mnist(100)
 
 X_train = mnist["training"]["data"]
 y_train = mnist["training"]["labels"]
 X_test = mnist["testing"]["data"]
 y_test = mnist["testing"]["labels"]
 
-weights = np.array(
-    (np.random.random_sample(size=54880).reshape(70, 784),
-     np.random.random_sample(size=1400).reshape(20, 70),
-     np.random.random_sample(size=200).reshape(10, 20)))
-biases = np.array(
-    (np.random.random_sample(size=(70, 1)),
-     np.random.random_sample(size=(20, 1)),
-     np.random.random_sample(size=(10, 1))))
-activations = np.array(
-    (np.zeros(shape=(784, 1)),
-     np.zeros(shape=(70, 1)),
-     np.zeros(shape=(20, 1)),
-     np.zeros(shape=(10, 1))))
+weight = np.array((np.random.uniform(low=-1, high=1, size=54880).reshape(70, 784),  # weights are a random number
+                   np.random.uniform(low=-1, high=1, size=1400).reshape(20, 70),    # between 1 and 0
+                   np.random.uniform(low=-1, high=1, size=200).reshape(10, 20)))
+bias = np.array((np.random.uniform(low=0, high=1, size=(70, 1)),   # biases are a random number between 1 and 0
+                 np.random.uniform(low=0, high=1, size=(20, 1)),   # implementing all column vectors as
+                 np.random.uniform(low=0, high=1, size=(10, 1))))  # number x 1 matrices
+activation = np.array((np.zeros(shape=(784, 1)),
+                       np.zeros(shape=(70, 1)),
+                       np.zeros(shape=(20, 1)),
+                       np.zeros(shape=(10, 1))))
 
-def feedforward():
+
+def normalize_inputs(data):
+    return data / 255
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def feed_forward(X):
+    activation[0] = normalize_inputs(X).reshape(784, 1)
+
+    for x in range(0, 2+1):  # loop from the first layer to the one before the last (as I will calc next activations)
+        weighted_sum = np.dot(weight[x], activation[x]) + bias[x]
+        activation[x+1] = sigmoid(weighted_sum)
+
+
+feed_forward(X_train[0])
+
+print(activation[3])
